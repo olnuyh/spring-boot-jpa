65ec5ede160be64c7650857500e548575a1ebc27
김이현 <rihyun1126@gmail.com>
2024-03-04 00:21:44 +0900

    [Section 4] 상품 도메인 개발

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,4 +26,19 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    // 비즈니스 로직
+    // 재고 수량 증가
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    // 재고 수량 감소
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0){
+            throw new NotEnoughStockException("Need More Stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
